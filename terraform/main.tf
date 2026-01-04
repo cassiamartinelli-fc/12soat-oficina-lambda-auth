7e3ab40c84727638d4dc71b184a87f5838831b22
Cassia Martinelli <martinelli.cassia@gmail.com>
2026-01-04 10:49:52 -0300

    feat: adiciona S3 backend para Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # Backend S3 para armazenar estado do Terraform
+  backend "s3" {
+    bucket = "12soat-terraform-state-lambda"
+    key    = "lambda-auth/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
